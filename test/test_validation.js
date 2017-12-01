13edbe7b3b6ee0e59c3879619b2d6488f34530da
alex <alex@staticlibs.net>
2017-12-01 00:21:25 +0000

    browser compat

--- test/test_validation.js
+++ test/test_validation.js
@@ -1,4 +1,4 @@
-define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
 'use strict';
 
 var test = require('tape-compat');
@@ -62,4 +62,4 @@ describe("sprintfjs", function() {
     })
 })
 
-return module.exports;});
+require = requireOrig;});
